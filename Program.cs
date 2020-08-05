d8a9eba8f6974071335193c864d81edbdc54ac0e
s <thesobczaks@hotmail.com>
2020-08-05 16:34:26 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csharpTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "81111111111111111111111111111111111111";
+            string b = "80000000000000000000000000000000000001";
+            string c = "50";
+
+            System.Console.WriteLine(Add(a,b));
+            System.Console.WriteLine(Add(a,c));
+            System.Console.WriteLine(Add(c,b));
+        }
+
+        static string Add(string str1, string str2)
+        {
+            return "Please implement this method";
+        }
+    }
+}
